7bcaa42de3ec21cc24666f03995ee3d4c30a5834
David Batonda <99521514+dtonda8@users.noreply.github.com>
2024-01-31 07:51:04 +1100

    Revert "Add Instructions + op_bridge fork"
    This reverts commit 4c84e8f16215cf35dfd2fc3601552530e8c6f934.

--- op_bridge/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ absolute import
-import sys
-__future_module__ = True
-
-if sys.version_info[0] < 3:
-    from Queue import *
-else: 
-    raise ImportError('This package shoud not be accessible on Python 3.')
